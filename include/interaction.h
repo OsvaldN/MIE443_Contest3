111c53255d140a69eefc268156fdd007c67bdb0a
David-Rolko <77452568+David-Rolko@users.noreply.github.com>
2021-04-06 20:24:23 -0400

    Made template for interaction function

--- include/interaction.h
+++ include/interaction.h
@@ -3,6 +3,6 @@
 
 #include "contest3.h"
 
-extern void Interact(int emotion, ros::Publisher *vel_pub, bool verbose = false);
+extern void Interact(int emotion, ros::Publisher *vel_pub, std::string path_to_sounds, bool verbose = false);
 
 #endif
